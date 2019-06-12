7f8a1b47c82891a6c11ca72a79d8d9089a6e3438
Carlo Gilmar <carlogilmar12@gmail.com>
2019-06-12 03:19:49 -0500

    Adding rules for write the answer

--- elixir_fizz/lib/bot.ex
+++ elixir_fizz/lib/bot.ex
@@ -1,19 +1,21 @@
 defmodule ElixirFizz.Bot do
 
   def answer({_fizzbot, false}), do: "Elixir"
-  def answer({fizzbot, true}), do: generate_answer(fizzbot["numbers"])
+  def answer({fizzbot, true}), do: generate_answer(fizzbot)
 
-  defp generate_answer(numbers) do
+  defp generate_answer(fizzbot) do
+    numbers = fizzbot["numbers"]
+    [numberA, numberB] = fizzbot["rules"]
     tokens =
       for number <- numbers do
-        fizzbuzz({number, rem(number, 3)== 0, rem(number, 5) == 0})
+        fizzbuzz({number, rem(number, numberA["number"])== 0, numberA["response"], rem(number, numberB["number"]) == 0, numberB["response"]})
       end
     Enum.join(tokens, " ")
   end
 
-  defp fizzbuzz({number, false, false}), do: "#{number}"
-  defp fizzbuzz({_number, true, false}), do: "Fizz"
-  defp fizzbuzz({_number, false, true}), do: "Buzz"
-  defp fizzbuzz({_number, true, true}), do: "FizzBuzz"
+  defp fizzbuzz({number, false, _numberA, false, _numberB}), do: "#{number}"
+  defp fizzbuzz({_number, true, numberA, false, _numberB}), do: numberA
+  defp fizzbuzz({_number, false, _numberA, true, numberB}), do: numberB
+  defp fizzbuzz({_number, true, numberA, true, numberB}), do: "#{numberA}#{numberB}"
 
 end
